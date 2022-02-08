b6f37a0b62de82df8f1e213517dee3c6bf91d2f5
Ayan-thecodeking <guptajiayan111@gmail.com>
2022-02-08 08:30:13 -0800

    added bottom navigation

--- BottomNavigationView/app/src/main/java/com/example/bottomnavigationview/MainActivity.kt
+++ BottomNavigationView/app/src/main/java/com/example/bottomnavigationview/MainActivity.kt
@@ -1,30 +1,61 @@
 package com.example.bottomnavigationview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.bottomnavigationview.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navigationView : BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        navigationView  = findViewById(R.id.bottomNavigationView)
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
-        val navController = navHostFragment.navController
-
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        title = resources.getString(R.string.favourite)
+        loadFragment(FirstFragment())
 
 
-        val appbarConfiguration= AppBarConfiguration(setOf(R.id.firstFragment,R.id.secondFragment,R.id.thirdFragment))
-        setupActionBarWithNavController(navController,appbarConfiguration)
+       navigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.firstFragment -> {
+                   title = "firstFragment"
+                    loadFragment(FirstFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
 
-       bottomNavigationView.setupWithNavController(navController)
+                R.id.secondFragment -> {
+                    title = "secondFragment"
+                    loadFragment(SecondFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+                R.id.thirdFragment -> {
+                    title = "thirdFragment"
+                    loadFragment(ThirdFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.fourthFragment -> {
+                    title = "fourthFragment"
+                    loadFragment(fourthFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+
+            }
+            false
+
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        // load fragment
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragmentContainerView, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
